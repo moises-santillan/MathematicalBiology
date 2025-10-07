884ff15d658cd34aaa898d1d0752fe1006afdb8c
Moisés Santillán <moises.santillan.zeron@gmail.com>
2025-10-07 16:44:51 -0600

    update

--- code/Ch13.py.py
+++ code/Ch13.py.py
@@ -11,26 +11,35 @@ def lotka_volterra(t, r, alpha, beta, rho):
     dy = rho * y * (1 - y - alpha * x)
     return dx, dy
 
+def transform(u, v):
+    arrow_lengths = np.sqrt(u*u + v*v)
+    len_adjust_factor = np.log(arrow_lengths + 1) / (arrow_lengths+1e-10)
+    return u*len_adjust_factor, v*len_adjust_factor
+
 # Set parameter values
 alpha_init = 1.5
 beta_init = 1.5
 rho_init = 1.0
 
 # Set up the grid
-x = np.linspace(0, 1.2, 20)
-y = np.linspace(0, 1.2, 20)
+x = np.linspace(0, 1.05, 16)
+y = np.linspace(0, 1.05, 16)
 X, Y = np.meshgrid(x, y)
+U =  X * (1 - X - beta_init * Y)
+V = rho_init * Y * (1 - Y - alpha_init * X)
+U2, V2 = transform(U, V)
+
 
 # Create the figure and axis
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.35)  # Adjust the position of the sliders
 
 # Set the axis limits
-ax.set_xlim([-0.1, 1.3])
-ax.set_ylim([-0.1, 1.3])
+ax.set_xlim([-0.05, 1.1])
+ax.set_ylim([-0.05, 1.1])
 
 # Plot the vector field
-quiver = ax.quiver(X, Y, X * (1 - X - beta_init * Y), rho_init * Y * (1 - Y - alpha_init * X))
+quiver = ax.quiver(X, Y, U2, V2, color="C0", scale=5)
 
 # Plot the steady states
 ax.plot([0, 0, 1, (1 - beta_init) / (1 - alpha_init * beta_init)],
@@ -38,8 +47,8 @@ ax.plot([0, 0, 1, (1 - beta_init) / (1 - alpha_init * beta_init)],
         'o', color="C1")
 
 # Add labels and title
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+ax.set_xlabel(r'$x_1$')
+ax.set_ylabel(r'$x_2$')
 ax.set_title('Lotka-Volterra Model - Vector Field of Derivatives')
 
 # Make axis equally sized
@@ -47,13 +56,13 @@ ax.set_aspect('equal')
 
 # Create sliders
 ax_alpha = plt.axes([0.25, 0.2, 0.65, 0.03])
-slider_alpha = Slider(ax_alpha, 'Alpha', 0.1, 3.0, valinit=alpha_init)
+slider_alpha = Slider(ax_alpha, r'$\alpha_{1,2}$', 0.1, 3.0, valinit=alpha_init)
 
 ax_beta = plt.axes([0.25, 0.15, 0.65, 0.03])
-slider_beta = Slider(ax_beta, 'Beta', 0.1, 3.0, valinit=beta_init)
+slider_beta = Slider(ax_beta, r'$\alpha_{2,1}$', 0.1, 3.0, valinit=beta_init)
 
 ax_rho = plt.axes([0.25, 0.1, 0.65, 0.03])
-slider_rho = Slider(ax_rho, 'Rho', 0.1, 3.0, valinit=rho_init)
+slider_rho = Slider(ax_rho, r'$\rho$', 0.1, 3.0, valinit=rho_init)
 
 # Update the vector field when sliders are changed
 def update(val):
